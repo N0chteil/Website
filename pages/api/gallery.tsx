e3f5893dc584918ea140eab0736d44f02f169d20
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-08-04 23:45:22 +0200

    Fix error

--- pages/api/gallery.tsx
+++ pages/api/gallery.tsx
@@ -1,10 +1,16 @@
-import fs from "fs";
-import path from "path";
-
 import { getGallery } from "../../lib/getGallery";
 
 export default async (req: any, res: any) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(await getGallery()));
+
+    let gallery = await getGallery();
+
+    try {
+        gallery = JSON.stringify(gallery);
+    } catch (e) {
+        gallery = {};
+    }
+
+    res.end(gallery);
 };
