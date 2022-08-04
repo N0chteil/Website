bf39f8347409180d0f887affeeab19609cf03047
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-08-05 00:06:17 +0200

    Vercel wtf

--- lib/getGallery.tsx
+++ lib/getGallery.tsx
@@ -1,5 +1,9 @@
 export async function getGallery() {
-    return await fetch(
+    const gallery = await fetch(
         "https://cdn.zephra.cloud/image/other/frederik/gallery/"
     ).then((res) => res.json());
+
+    console.log("getGallery", gallery);
+
+    return gallery;
 }
