77e22ce0fcaf18c1e97626ba2ded80487ef9722b
Frederik <53608074+N0chteil@users.noreply.github.com>
2023-04-12 15:40:28 +0200

    chore: add function name

--- pages/api/gallery.tsx
+++ pages/api/gallery.tsx
@@ -1,16 +1,8 @@
 import { getGallery } from "../../lib/getGallery";
 
-export default async (req: any, res: any) => {
+export default async function handler(req: any, res: any) {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
 
-    let gallery = await getGallery();
-
-    try {
-        gallery = JSON.stringify(gallery);
-    } catch (e) {
-        gallery = {};
-    }
-
-    res.end(gallery);
+    res.end(await getGallery());
 };
