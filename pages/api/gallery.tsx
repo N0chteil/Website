06ac4919c2ee8022e40dcac5c7965a0fd333a79f
Frederik <53608074+N0chteil@users.noreply.github.com>
2023-04-12 15:42:19 +0200

    fix: ups

--- pages/api/gallery.tsx
+++ pages/api/gallery.tsx
@@ -4,5 +4,15 @@ export default async function handler(req: any, res: any) {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
 
-    res.end(await getGallery());
+    let gallery = await getGallery();
+
+    try {
+        gallery = JSON.stringify(gallery);
+    } catch (e) {
+        console.error(e);
+
+        gallery = {};
+    }
+
+    res.end(gallery);
 };
