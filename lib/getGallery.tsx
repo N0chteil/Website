d9a7a71ce9c0d2eee7ae75d918e05735491e1271
Frederik <53608074+N0chteil@users.noreply.github.com>
2023-04-12 15:45:03 +0200

    feat: add console logs

--- lib/getGallery.tsx
+++ lib/getGallery.tsx
@@ -11,11 +11,13 @@ export interface GalleryDataItem {
 export async function getGallery() {
     let gallery: any = await fetch(
         "https://gallery.frederik.life"
-    ).then((res) => res.text());
+    ).then((res) => res.text()).catch((e) => console.error(e));
 
     try {
         gallery = JSON.parse(gallery);
     } catch (e) {
+        console.error(e);
+
         gallery = {};
     }
 
